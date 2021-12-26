eb99c24d447fea659abd0eaf1a45151590ab3dfa
Victor Nwogu <vicmeee76@gamil.com>
2021-12-26 13:57:11 +0100

    -- code update

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -169,6 +169,7 @@ exports._showProducts = (code, res, msg, response) => {
                 ProductName: x.ProductName,
                 Price: "$" + getPrice(x.SellingPrice, x.Discount),
                 Discount: x.Discount <= 0 ? "" : x.Discount + "% Off",
+                OldPrice: x.Discount <= 0 ? "" : "$" + parseFloat(x.SellingPrice),
                 ProductImage: x.ProductImages.split(",")[0],
                 Expiry: getProductExpiry(x.ExpiredAt),
                 ViewProducts: {
@@ -209,7 +210,7 @@ exports._showSingleProducts = (code, res, msg, response) => {
                 Sku: x.Sku,
                 Price: "$" + getPrice(x.SellingPrice, x.Discount),
                 Discount: x.Discount <= 0 ? "" : x.Discount + "% Off",
-                OldPrice: x.Discount <= 0 ? "" : parseFloat(x.SellingPrice),
+                OldPrice: x.Discount <= 0 ? "" : "$" + parseFloat(x.SellingPrice),
                 StockLevel: x.StockLevel,
                 ProductImages: generateArr(x.ProductImages),
                 Colors: generateArr(x.Colors),
