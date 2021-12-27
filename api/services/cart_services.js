ed7f244e99cdb36180c886237aeb45c8484c7b87
Victor Nwogu <vicmeee76@gamil.com>
2021-12-27 18:10:11 +0100

    -- implement CRUD for cart module

---
+++ api/services/cart_services.js
@@ -0,0 +1,58 @@
+const pool = require("../../config/database.js");
+const helpers = require("../../helpers/helpers");
+
+module.exports = {
+
+    getCart: (id, callBack) => {
+
+        pool.query("SELECT * FROM cart WHERE UserId = ?",
+            [
+                id
+            ],
+            (error, result) => {
+                helpers._getCallBack(callBack, error, result);
+            });
+    },
+
+
+    saveCart: (data, callBack) => {
+
+        pool.query("INSERT INTO cart(UserId, ProductId, Qty) VALUES(?,?,?)",
+            [
+                data.ProductId,
+                data.UserId,
+                data.Qty,
+            ],
+            (error, result) => {
+                helpers._getCallBack(callBack, error, result);
+            });
+    },
+
+
+
+    updateCart: (id, data, callBack) => {
+
+        pool.query("UPDATE cart SET Qty=? WHERE CartId=?",
+            [
+                data.Qty,
+                id
+            ],
+            (error, result) => {
+                helpers._getCallBack(callBack, error, result);
+            });
+    },
+
+
+
+    deleteCart: (id, callBack) => {
+        pool.query("DELETE FROM cart WHERE CartId=?",
+            [
+                id
+            ],
+            (error, result) => {
+                helpers._getCallBack(callBack, error, result);
+            });
+    },
+
+
+}
