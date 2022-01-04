827515214c72cb5113ebe08cc452bae4b5376148
Victor Nwogu <vicmeee76@gamil.com>
2022-01-04 13:18:01 +0100

    -- optimaisation for category module

--- api/controllers/category_controller.js
+++ api/controllers/category_controller.js
@@ -11,163 +11,141 @@ const {
 } = require("../services/category_services");
 
 
+// create new category
+exports._saveCategory = async (req, res) => {
+    try {
+        const data = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-exports._saveCategory = (req, res, next) => {
-    const data = req.body;
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        const check = await checkExisitingCategory(null, data);
+      
+        if (check && check.length > 0) {
+            helpers._showError(409, res, "This category name already exits.");
+        }
+        else {
+            const save = await saveCategory(data);
+            if (save) {
+                helpers._showSuccess(201, res, "Category created successfully", save);
+            }
+            else {
+                helpers._showError(500, res, "Something went wrong");
+            }
+        }
+    } catch (e) {
+        helpers._showError(500, res, e);
     }
-
-    checkExisitingCategory(null, data, (err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
-        }
-        else {
-            if (results && results.length > 0) {
-                helpers._showError(409, res, "This category name already exits.");
-            }
-            else {
-                saveCategory(data, (errs, response) => {
-                    if (errs) {
-                        helpers._showError(500, res, errs);
-                    }
-                    else {
-                        helpers._showSuccess(201, res, "Category created successfully", response);
-                    }
-                });
-            }
-        }
-    });
 };
 
 
-exports._getCategories = (req, res, next) => {
+// get all categories
+exports._getCategories = async (req, res) => {
 
-    getCategories((err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
+    try {
+        const get = await getCategories();
+        if (get && get.length > 0) {
+            helpers._showAllCategories(200, res, "Catgories found", get);
         }
         else {
-            if (results) {
-                helpers._showAllCategories(200, res, "Catgories found", results);
-            }
-            else {
-                helpers._showError(404, res, "Category not found");
-            }
+            helpers._showError(404, res, "Category not found");
         }
-    });
-};
-
-
-exports._getCategoryById = (req, res, next) => {
-
-    const categoryId = req.params.id;
-    getCategoriesById(categoryId, (err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
-        }
-        else {
-            if (results && results.length > 0) {
-                helpers._showSingleCategory(200, res, "Catgories found", results);
-            }
-            else {
-                helpers._showError(404, res, "Category not found");
-            }
-        }
-    });
-};
-
-
-exports._getCategoryProducts = (req, res, next) => {
-
-    const categoryId = req.params.id;
-
-    getCategoryProducts(categoryId, (err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
-        }
-        else {
-            if (results && results.length > 0) {
-                helpers._showSingleCategory(200, res, "Products found for this category", results);
-            }
-            else {
-                helpers._showError(404, res, "Products for this category not found");
-            }
-        }
-    });
-};
-
-
-exports._updateCategory = (req, res, next) => {
-
-    const categoryId = req.params.id;
-    const data = req.body;
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
     }
-
-    checkExisitingCategory(categoryId, data, (err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
-        }
-        else {
-            if (results && results.length > 0) {
-
-                updateCategory(id, data, (err, results) => {
-                    if (err) {
-                        helpers._showError(500, res, err);
-                    }
-                    else {
-                        if (results) {
-                            helpers._showSuccess(201, res, "Category updated successfully", results);
-                        }
-                        else {
-                            helpers._showError(304, res, "Not modified, something went wrong");
-                        }
-                    }
-                });
-            }
-            else {
-                helpers._showError(404, res, "Category not found");
-            }
-        }
-    });
+    catch (e) {
+        helpers._showError(500, res, e);
+    }
 };
 
 
-exports._deleteCategory = (req, res, next) => {
 
-    const categoryId = req.params.id;
-    
-    getCategoriesById(categoryId, (err, results) => {
-        if (err) {
-            helpers._showError(500, res, err);
+// get category by category id
+exports._getCategoryById = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const get = await getCategoriesById(categoryId);
+        
+        if (get) {
+            helpers._showSingleCategory(200, res, "Catgories found", get);
         }
         else {
-            if (results && results.length > 0) {
-                deleteCategory(id, (err, results) => {
-                    if (err) {
-                        helpers._showError(500, res, err);
-                    }
-                    else {
-                        if (results) {
-                            helpers._showSuccess(200, res, "Category deleted successfully", results);
-                        }
-                        else {
-                            helpers._showError(501, res, "Something went wrong " + err);
-                        }
-                    }
-                });
+            helpers._showError(404, res, "Category not found");
+        }
+    } catch (e) {
+        helpers._showError(500, res, e);
+    }
+};
+
+
+// get products in a category
+exports._getCategoryProducts = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const cat = await getCategoryProducts(categoryId);
+
+        if (cat && cat.length > 0) {
+            helpers._showSingleCategory(200, res, "Products found for this category", cat);
+        }
+        else {
+            helpers._showError(404, res, "Products for this category not found");
+        }
+    } catch (e) {
+        helpers._showError(500, res, e);
+    }
+};
+
+
+
+// update a category details
+exports._updateCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const data = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const check = await checkExisitingCategory(categoryId, data);
+
+        if (check && check.length > 0) {
+            const update = await updateCategory(categoryId, data);
+            if (update) {
+                helpers._showSuccess(201, res, "Category updated successfully", update);
             }
             else {
-                helpers._showError(404, res, "Category cannot be found");
+                helpers._showError(304, res, "Not modified, something went wrong");
             }
         }
-    });
+        else {
+            helpers._showError(404, res, "Category not found");
+        }
+    } catch (e) {
+        helpers._showError(500, res, e);
+    }
+};
+
+
+// delete category
+exports._deleteCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const get = await getCategoriesById(categoryId);
+
+        if (get && get.length > 0) {
+            const del = await deleteCategory(categoryId);
+            if (del) {
+                helpers._showSuccess(200, res, "Category deleted successfully", del);
+            }
+            else {
+                helpers._showError(501, res, "Something went wrong ");
+            }
+        }
+        else {
+            helpers._showError(404, res, "Category cannot be found");
+        }
+    } catch (e) {
+        helpers._showError(500, res, e);
+    }
 }
 
 
